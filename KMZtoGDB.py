f8ba288afe5fc31439ec5dde489c67902214b3a4
Scott Howard <scott@showard.ca>
2021-07-23 11:41:47 -0700

    successful import of KMZ into geodatabase. still need to move images, verify feature is not already in gdb, and create layer file from KMZ styles

--- KMZtoGDB.py
+++ KMZtoGDB.py
@@ -5,18 +5,140 @@ import os
 import re
 import datetime
 import argparse
-from pyproj import Transformer
+import arcpy
 
 RE_COORDS = re.compile('(-?[0-9]+\.[0-9]+),([0-9]+\.[0-9]+),')
-#RE_DATE = re.compile('([0-9][0-9][0-9][0-9])-([0-9][0-9])-([0-9][0-9])T([0-9][0-9]):([0-9][0-9]):([0-9][0-9])')
+RE_DATE = re.compile('([0-9][0-9][0-9][0-9])-([0-9][0-9])-([0-9][0-9])T([0-9][0-9]):([0-9][0-9]):([0-9][0-9])')
 RE_IMG = re.compile('<img src="(images[^"]*)"')
-transformer = Transformer.from_crs(4326, 3005, always_xy=True)
+
+class gdbAccess:
+    ProjectFolder = 'G:/Projects/Various_Clients/Galore Creek/Field Data'
+    GDB = ProjectFolder + '/testing.gdb'
+    photo_dir = ProjectFolder + '/KMZPhotos'
+
+    fc_points = GDB + '/KMLPoint'
+    attr_points = ['Name', 'Description', 'Date', 'Origin_file', 'SHAPE@']
+
+    fc_lines = GDB + '/KMLLine'
+    attr_lines = ['Name', 'Description', 'Date', 'Origin_file', 'SHAPE@']
+
+    table_photos = GDB + '/Photos'
+    attr_photos = ['Feature_Type', 'Feature_OBJECTID', 'Photo_Path']
+
+    def __init__(self):
+        self.editor = arcpy.da.Editor(self.GDB)
+
+    def startEditing(self):
+        self.editor.startEditing()
+        self.editor.startOperation()
+        self.cursor = {'Point':    arcpy.da.InsertCursor (self.fc_points, self.attr_points),
+                       'Polyline': arcpy.da.InsertCursor (self.fc_lines, self.attr_lines),
+                       'Photos':   arcpy.da.InsertCursor (self.table_photos, self.attr_photos)}
+
+    def insertRow (self, feature):
+        try: 
+            objectid = self.cursor[feature.featureType].insertRow(feature.toTuple())
+        except RuntimeError:
+            print ("Error inserting new row: " + str(feature.toTuple()))
+            raise
+        if len(feature.photos) > 0:
+            for photo in feature.photos:
+                # set photopath to be <kmz filename w/o .kmz>_<photo name>.jpg
+                photopath = feature.filename[:-4] + '_' + photo[len('images/'):]
+                try:
+                    self.cursor['Photos'].insertRow ((feature.featureType, objectid, photopath))
+                except RuntimeError:
+                    print ("Error inserting photos: " + photopath)
+                    raise
+    
+    def stopEditing (self, commit=False):
+        self.editor.stopOperation()
+        self.editor.stopEditing(commit)
+
+    def __del__ (self):
+        if self.editor.isEditing:
+            self.editor.stopEditing(False)
+
+class Feature:
+    gcs_sr = arcpy.SpatialReference(4326) # Lat Long: Geographic Coordinate System WGS84
+    albers_sr = arcpy.SpatialReference(3005) # Albers
+
+    def __init__(self, dom, filename):
+        self.name = getName(dom)
+        self.date = getDate(dom)
+        self.description, self.photos = getExtendedData(dom)
+        self.featureType, self.shape = getShape(dom)
+        self.filename = filename
+
+    def toTuple(self):
+        return (self.name, self.description, self.date, self.filename, self.shape)
+
+    def getName(feature):
+        name = feature.getElementsByTagName("name")[0].childNodes[0].data
+
+        prefix="<![CDATA["
+        suffix="]]>"
+        
+        if name.startswith(prefix):
+            name = name[len(prefix):]
+
+        if name.endswith(suffix):
+            name = name[:-len(suffix)]
+
+        return name
+
+    def getDate(feature):
+        raw = feature.getElementsByTagName("when")[0].childNodes[0].data
+        match = RE_DATE.search(raw)
+        return datetime.datetime(int(match.group(1)),
+                                 int(match.group(2)),
+                                 int(match.group(3)),
+                                 int(match.group(4)),
+                                 int(match.group(5)),
+                                 int(match.group(6)))
+
+    def getShape(feature):
+        raw = feature.getElementsByTagName("coordinates")[0].childNodes[0].data
+
+        match = RE_COORDS.findall(raw, re.MULTILINE)
+        points = []
+        for coord in match:
+            points.append(arcpy.Point(coord[0], coord[1]))
+
+        if len(points) == 1:
+            featureType = "Point"
+            feature = arcpy.PointGeometry(points[0], gcs_sr)
+        else:
+            featureType = "Polyline"
+            feature = arcpy.Polyline(arcpy.Array(points), gcs_sr)
+
+        projectedFeature = feature.projectAs(albers_sr)
+
+        return featureType, projectedFeature
+
+    def getPhotoList(s):
+        raw = s.childNodes[0].data
+        return RE_IMG.findall(raw)
+
+    def getExtendedData(feature):
+        description = ""
+        photos = []
+
+        simpledata = feature.getElementsByTagName("SimpleData")
+
+        for s in simpledata:
+            if s.getAttribute("name") == "Description":
+                description = s.childNodes[0].data
+            elif s.getAttribute("name") == "pdfmaps_photos":
+                photos = getPhotoList(s)
+
+        return description, photos
 
 def openKMZ(filename):
     if os.access (filename, os.R_OK):
         return zipfile.ZipFile(filename, 'r')
     else:
-        print (f'{sys.argv[0]}: could not open KMZ: {filename}', file=sys.stderr)
+        print ('KMZtoGDB: could not open KMZ: ' + filename)
         sys.exit(1)
 
 def openDOM (kmz):
@@ -24,87 +146,30 @@ def openDOM (kmz):
     if kml in kmz.namelist():
         return parseDOM(kmz.open(kml))
     else:
-        print (f'{sys.argv[0]}: invalid KMZ.', file=sys.stderr)
+        print ('KMZtoGDB: invalid KMZ.')
         sys.exit(1)
 
-def getName(feature):
-    name = feature.getElementsByTagName("name")[0].childNodes[0].data
+def processKMZ(kmzDOM, filename):
+    domPlacemarks = kmzDOM.getElementsByTagName("Placemark")
+    gdb = gdbAccess ()
+    gdb.startEditing()
 
-    prefix="<![CDATA["
-    suffix="]]>"
-    
-    if name.startswith(prefix):
-        name = name[len(prefix):]
+    for dom in domPlacemarks:
+        feature = Feature(dom, filename)
+        gdb.insertRow(feature)
 
-    if name.endswith(suffix):
-        name = name[:-len(suffix)]
+        #print ('Feature is type: ' + feature.featureType)
+        #print ('\t  name:\t' + feature.name)
+        #print ('\t  date:\t' + feature.date.isoformat())
+        #print ('\t  desc:\t' + feature.description)
+        #print ('\tphotos:\t' + str(feature.photos))
+        #print ('\tcoords:\t' + str(feature.shape.firstPoint.X) + ',' + str(feature.shape.firstPoint.Y))
 
-    return name
+    gdb.stopEditing(True)
 
-def getDate(feature):
-    raw = feature.getElementsByTagName("when")[0].childNodes[0].data
-    return datetime.datetime.fromisoformat(raw)
+def KMZtoGDB(**kwargs):
+    filename = kwargs.get('kmz')
+    kmz = openKMZ(filename)
+    kmzDOM = openDOM(kmz)
 
-def getCoords(feature):
-    raw = feature.getElementsByTagName("coordinates")[0].childNodes[0].data
-
-    match = RE_COORDS.findall(raw, re.MULTILINE)
-    xyCoords = []
-    for coord in match:
-        xyCoords.append(transformer.transform(coord[0], coord[1]))
-
-    return xyCoords
-
-def getPhotoList(s):
-    raw = s.childNodes[0].data
-    return RE_IMG.findall(raw)
-
-def getExtendedData(feature):
-    description = ""
-    photos = []
-
-    simpledata = feature.getElementsByTagName("SimpleData")
-
-    for s in simpledata:
-        if s.getAttribute("name") == "Description":
-            description = s.childNodes[0].data
-        elif s.getAttribute("name") == "pdfmaps_photos":
-            photos = getPhotoList(s)
-
-    return description, photos
-
-def printFeature(feature):
-    featureType = ""
-    if len(feature.getElementsByTagName("Point")) > 0:
-        featureType = "point"
-    elif len(feature.getElementsByTagName("LineString")) > 0:
-        featureType = "line"
-    else:
-        print (f'{sys.argv[0]}: unknown feature:\n{feature.toxml()}', file=sys.stderr)
-        return
-
-    name = getName(feature)
-    datetime = getDate(feature)
-    coords = getCoords(feature)
-    description, photos = getExtendedData(feature) 
-
-    print (f'type: {featureType}, name: {name}, datetime: {datetime}, coords: {coords}, description: {description}, photos: {photos}')
-
-def printFeatures(dom):
-    features = dom.getElementsByTagName("Placemark")
-
-    for f in features:
-        printFeature(f)
-
-def main(**kwargs):
-    kmz = openKMZ(kwargs.get('kmz'))
-    dom = openDOM(kmz)
-
-    printFeatures(dom)
-
-if __name__ == '__main__':
-    p = argparse.ArgumentParser()
-    p.add_argument('--kmz', dest='kmz', action='store', required=True,
-        metavar='file_path', help='path to the KMZ to open')
-    args = p.parse_args()
-    main(**vars(args))
+    processKMZ(kmzDOM, os.path.basename(filename))
