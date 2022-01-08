280c9ca651df2e2c32adb60c1f62f5d3e3cce666
Scott Howard <scott@showard.ca>
2022-01-08 13:00:11 -0800

    improved script to include grade in attributes

--- InsertSteepGrades.py
+++ InsertSteepGrades.py
@@ -3,19 +3,19 @@ import arcpy
 import datetime
 
 def uniqueNumber():
-    return datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    return datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')
 
 albers=arcpy.SpatialReference(3005)
 utm=arcpy.SpatialReference(26909)
 
-file=open('N:/Strategic_Group/Projects/21-1547-30 Galore Creek Mine Access Study/Phase/GeoData_Working/Anuk_Grades.csv','r')
+file=open('N:/Strategic_Group/Projects/21-1547-30 Galore Creek Mine Access Study/Phase/GeoData_Working/grades.csv','r')
 reader=csv.reader(file)
 rows=[]
 for row in reader:
     rows.append(row)
 file.close()
 
-rows=rows[2:]
+rows=rows[1:]
 
 gdb='G:/Projects/Various_Clients/Galore Creek/WorkingData.gdb'
 arcpy.env.workspace = gdb
@@ -39,6 +39,16 @@ def cleanup():
             raise ("problem cleaning up")
 
 
+class SteepSegment:
+    def __init__(self, road, grade, start, end):
+        self.roadName = None
+        self.roadCode = road
+        self.grade = grade
+        self.start = start
+        self.end = end
+        self.polyline = None
+
+
 #########
 ### Insert split points into temp feature class
 
@@ -51,27 +61,25 @@ try:
     editor.startEditing()
     editor.startOperation()
     cursor=arcpy.da.InsertCursor(fc_splitpoints,['SHAPE@'])
-    inSteepSegment=False
+    splitPoints = []
     for row in rows:
-        x=row[0]
-        y=row[1]
-        point=arcpy.PointGeometry(arcpy.Point(x, y), utm).projectAs(albers)
+        road=row[0]
+        grade=abs(int(row[1]))
+        startX=row[2]
+        startY=row[3]
+        endX=row[4]
+        endY=row[5]
+        start=arcpy.PointGeometry(arcpy.Point(startX, startY), utm).projectAs(albers)
+        end=arcpy.PointGeometry(arcpy.Point(endX, endY), utm).projectAs(albers)
 
-        if row[2] == '':
-            break
+        if start not in splitPoints:
+            splitPoints.append(start)
+            cursor.insertRow(start)
+        if end not in splitPoints:
+            splitPoints.append(end)
+            cursor.insertRow(end)
 
-        grade=abs(int(row[2]))
-        if not inSteepSegment and grade <= 20:
-            continue
-        elif inSteepSegment and grade > 20:
-            continue
-        elif not inSteepSegment and grade > 20:
-            inSteepSegment = True
-            steepSegments.append([point])
-        elif inSteepSegment and grade <= 20:
-            inSteepSegment = False
-            steepSegments[-1].append(point)
-        cursor.insertRow(point)
+        steepSegments.append(SteepSegment(road, grade, start, end))
 
     editor.stopOperation()
     editor.stopEditing(True)
@@ -87,7 +95,7 @@ except:
 
 fc_splitroad = 'temp_{}'.format(uniqueNumber())
 try:
-    arcpy.SplitLineAtPoint_management('FEL2A_LLine_Prelim', fc_splitpoints, fc_splitroad, "1 Meter")
+    arcpy.SplitLineAtPoint_management('FEL2A_LLine_MAPPING_PURPOSES', fc_splitpoints, fc_splitroad, "1 Meter")
     cleanup_fcs.append(fc_splitroad)
 except:
     editor.stopOperation()
@@ -113,20 +121,29 @@ search = arcpy.da.SearchCursor(fc_splitroad, ['SHAPE@','Road_Name','Road_Code'])
 steepPolylines = []
 for steepSegment in steepSegments:
     for segment in search:
-        if isCoincident(segment[0], steepSegment):
-            steepPolylines.append(segment)
+        if isCoincident(segment[0], (steepSegment.start, steepSegment.end)):
+            steepSegment.polyline = segment[0]
+            steepSegment.roadName = segment[1]
+            steepPolylines.append(steepSegment)
     search.reset()
 
 #########
 ### Insert steep segments into FEL2A_LLine_SteepGrades
 
-fc_steepgrades = 'FEL2A_LLine_SteepGrades'
+fc_steepgrades_anuk = 'FEL2A_LLine_SteepGrades_Anuk'
+fc_steepgrades_asr = 'FEL2A_LLine_SteepGrades'
 try:
     editor.startEditing()
     editor.startOperation()
-    cursor=arcpy.da.InsertCursor(fc_steepgrades, ['SHAPE@','Road_Name','Road_Code'])
+    anuk_cursor=arcpy.da.InsertCursor(fc_steepgrades_anuk, ['SHAPE@','Road_Name','Road_Code','Grade'])
+    asr_cursor=arcpy.da.InsertCursor(fc_steepgrades_asr, ['SHAPE@','Road_Name','Road_Code','Grade'])
     for segment in steepPolylines:
-        cursor.insertRow (segment)
+        cursor = None
+        if segment.roadCode.startswith('AN'):
+            cursor = anuk_cursor
+        else:
+            cursor = asr_cursor
+        cursor.insertRow ((segment.polyline, segment.roadName, segment.roadCode, segment.grade))
     editor.stopOperation()
     editor.stopEditing(True)
 except:
